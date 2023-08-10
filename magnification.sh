9228b5de8203ba3b21d6bf249bd3c0ad91e63269
cpothin <138033354+Keftark@users.noreply.github.com>
2023-08-10 15:14:11 +0200

    Update magnification.sh

--- magnification.sh
+++ magnification.sh
@@ -9,4 +9,10 @@ gsettings set org.gnome.desktop.a11y.magnifier contrast-blue 1
 gsettings set org.gnome.desktop.a11y.magnifier contrast-red 1 
 gsettings set org.gnome.desktop.a11y.magnifier contrast-green 1
 gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled true
-echo "alias demagnification='gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled false'" >> ~/.zshrc
+alias_name="demagnification"
+alias_command="gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled false"
+if ! alias "$alias_name" &> /dev/null; then
+    # Define the alias
+    alias "$alias_name"="$alias_command"
+fi
+#echo "alias demagnification='gsettings set org.gnome.desktop.a11y.applications screen-magnifier-enabled false'" >> ~/.zshrc
